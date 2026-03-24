000d352b5a77bd3c1855a83dc594021fb97c4ff7
Molka-Bennouri <molkabennouri1s5@gmail.com>
2026-03-24 12:09:59 +0100

    add networks seeder

---
+++ database/seeders/NetworksSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NetworksSeeder extends Seeder
+{
+    public function run(): void
+    {
+        DB::table('networks')->truncate();
+
+        DB::table('networks')->insert([
+            [
+                'name'         => 'Community Health Network',
+                'slug_pattern' => '@^https?://(?:[\w-]+\.)*ecommunity\.[a-z.]+(?:/[^/]+)*/providers?/[A-Za-z]+/([0-9]+)/?$@i',
+            ],
+            [
+                'name'         => 'Kununu',
+                'slug_pattern' => '@^(?:(?:(?:https?://)?(?:www\.)?kununu\.com)?/)?(?:de/)?([a-z0-9-]+)@',
+            ],
+            [
+                'name'         => 'Viator',
+                'slug_pattern' => '@^https?://(?:www\.)?viator\.com/tours/[A-Za-z0-9-]+/[A-Za-z0-9-]+/d\d+-\d+[A-Z]\d+$@i',
+            ],
+        ]);
+    }
+}
