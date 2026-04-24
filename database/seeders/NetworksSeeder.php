3ac502c9ceb24ec634818460be4eb7a813e4e3ad
Molka-Bennouri <molkabennouri1s5@gmail.com>
2026-04-24 12:32:28 +0100

    add new networks

--- database/seeders/NetworksSeeder.php
+++ database/seeders/NetworksSeeder.php
@@ -23,9 +23,24 @@ class NetworksSeeder extends Seeder //php artisan db:seed --class=NetworksSeeder
                 'slug_pattern' => '@^https?://(?:[\w-]+\.)*ecommunity\.[a-z.]+/(?:[^/]+/)*providers?/[A-Za-z]+/([0-9]+)/?$@i',
             ],
             [
+                'name'         => 'justia',
+                'label'        => 'Justia',
+                'slug_pattern' => '@^(?:(?:https?://)?(?:[\w-]+\.)*justia(?:\.[a-z]{2,3}){1,2}/lawyer/)?([\w][\w-]*-\d+)(?:/[\w-]*)?(?:[?#].*)?$@i',
+            ],
+            [
                 'name'         => 'kununu',
                 'label'        => 'Kununu',
                 'slug_pattern' => '@^(?:(?:(?:https?://)?(?:www\.)?kununu\.com)?/)?(?:de/)?([a-z][a-z0-9-]*[a-z0-9])$@',
+            ],
+            [
+                'name'         => 'lawtally',
+                'label'        => 'Lawtally',
+                'slug_pattern' => '@^(?:(?:https?:\/\/)?(?:[\w-]+\.)*lawtally(?:\.[a-z]{2,3}){1,2}\/)?(?:lawyers\/)?([\w-]+)\/?(?:[?#].*)?$@i',
+            ],
+            [
+                'name'         => 'ssmhealth',
+                'label'        => 'SSM Health',
+                'slug_pattern' => '@^(?:https?://)?(?:[\w-]+\.)?getcare\.ssmhealth(?:\.[a-z]{2,3}){1,2}/?(?:find-a-doctor/doctor-details/|doctor-details/)?([a-z0-9]+(?:-[a-z0-9]+)*)/?(?:[?#].*)?$@i',
             ],
             [
                 'name'         => 'viator',
